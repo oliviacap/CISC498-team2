3c9376a3d7c24d7df4d4d2c6f9f022164635ac51
oliviacap <livcap@udel.edu>
2021-09-30 17:28:49 -0400

    adding welcome page

---
+++ maize-master/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  maize master
+//
+//  Created by Olivia Caponigro on 9/27/21.
+//
+
+import SwiftUI
+
+let backgroundColor = Color(UIColor.secondarySystemBackground)
+struct ContentView: View {
+    var body: some View {
+
+        VStack {
+//            Text("maize master")
+//                .font(.largeTitle)
+//                .fontWeight(.bold)
+//            .multilineTextAlignment(.center)
+//            Text("sparks lab")
+        }
+        }
+
+    }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ maize-master/maize_masterApp.swift
@@ -0,0 +1,19 @@
+//
+//  maize_masterApp.swift
+//  maize master
+//
+//  Created by Olivia Caponigro on 9/27/21.
+//
+
+import SwiftUI
+
+@main
+struct maize_masterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            WelcomeView()
+            
+        }
+    }
+}
