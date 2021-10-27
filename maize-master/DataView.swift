93c365f79c2c827305933661fd263046c75b2a88
oliviacap <livcap@udel.edu>
2021-10-27 17:45:13 -0400

    changed backgroud and text color

---
+++ maize-master/DataView.swift
@@ -0,0 +1,8 @@
+//
+//  DataView.swift
+//  maize-master
+//
+//  Created by Olivia Caponigro on 10/19/21.
+//
+
+import Foundation
--- maize-master/WelcomeView.swift
+++ maize-master/WelcomeView.swift
@@ -8,29 +8,41 @@
 import SwiftUI
 
 let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
+let maizeGreen = Color(red: 0.0/255.0, green: 51.0/255.0, blue: 0.0/255.0, opacity: 1.0)
+
 
 struct WelcomeView: View {
     @State var email: String = ""
     
     var body: some View {
-        //welcome screen
         VStack{
+
             WelcomeText()
             AppIcon()
             EmailField()
-            Button(action: {print("Button tapped")}){
+            Button(action: {print("Login Button tapped")}){
             LoginButton()
             }
             
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity) // 1
         .padding()
+        .background(Color("maizeGreen"))
+        .foregroundColor(Color("maizeGreen"))
+        //.padding()
+        // self.WelcomeText.backgroundColor = maizeGreen
+
         
     }
 }
 
 struct SwiftUIView_Previews: PreviewProvider {
     static var previews: some View {
+        
         WelcomeView()
+            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(red: 0.0, green: 0.2, blue: 0.0)/*@END_MENU_TOKEN@*/)
+            .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/)
+            
     }
 }
 
@@ -38,6 +50,7 @@ struct WelcomeText: View {
     var body: some View {
         Text("Welcome to maize master!")
             .font(.largeTitle)
+            .foregroundColor(.white)
             .fontWeight(.semibold)
             .padding(.bottom, 20)
             .multilineTextAlignment(.center)
@@ -60,6 +73,7 @@ struct EmailField: View{
         TextField("email",text: $email)
             .padding()
             .background(lightGreyColor)
+            .foregroundColor(.black)
             .cornerRadius(5.0)
             .padding(.bottom, 20)
     }
