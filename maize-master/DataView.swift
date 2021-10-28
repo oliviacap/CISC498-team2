3b4f15d26766e0f2364be45badda114b146bf616
oliviacap <livcap@udel.edu>
2021-10-28 14:34:40 -0400

    added dataview

--- maize-master/DataView.swift
+++ maize-master/DataView.swift
@@ -3,6 +3,19 @@
 //  maize-master
 //
 //  Created by Olivia Caponigro on 10/19/21.
-//
+// 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
+// https://github.com/spacenation/swiftui-charts.git.
+
 
 import Foundation
+import SwiftUI
+import Charts
+
+struct ContentView: View {
+    var body: some View {
+        Chart(data: [0.1, 0.3, 0.2, 0.5, 0.4, 0.9, 0.1])
+            .chartStyle(
+                LineChartStyle(.quadCurve, lineColor: .blue, lineWidth: 5)
+            )
+    }
+}
